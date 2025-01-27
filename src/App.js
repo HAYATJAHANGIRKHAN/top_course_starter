a58a436c5e9c76eab44d5ce86ed4fc3678bca8f0
Hayat <hayatjahangir0@gmail.com>
2025-01-27 19:26:22 +0530

    add cards and navebar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React from "react";
-import {apiUrl, filterData} from "./data";
+import React, { useEffect, useState } from "react";
+import { apiUrl, filterData } from "./data";
 import Navbar from "./Components/Navbar";
 import Filter from "./Components/Filter";
 import Cards from "./Components/Cards";
+import { toast } from "react-toastify";
 
 const App = () => {
-  return(
+  const [courses, setCourses] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        //save data into a variable
+        // console.log(data);
+        setCourses(data.data);
+      }
+      catch (error) {
+     toast.error(" something went wrong");
+      }
+    }
+    fetchData();
+  },[]);
+  return (
     <div>
-      <Navbar/>
+      <Navbar />
 
       <Filter
         filterData={filterData}
-        
+
       />
-      <Cards/>
+      <Cards />
     </div>
   )
 };
--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -4,12 +4,15 @@ const Filter = ({ filterData }) => {
     return (
         <div>
             {filterData.map((data) => {
+                return(
+
                 <button>
                     {data.title};
                 </button>
+                )
             })}
         </div>
     )
 }
 
-export default Filter
+export default Filter;
